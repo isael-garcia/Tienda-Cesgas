f72607ec2121af16a6004a08af655f19049b76a4
isael-garcia <69340389+isael-garcia@users.noreply.github.com>
2021-09-13 22:36:17 -0500

    agregando los primeros componentes de angular

--- tienda/src/app/app.module.ts
+++ tienda/src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
 
+import { routing } from "./app.routing";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { routing } from "./app.routing";
 import { InicioComponent } from './components/inicio/inicio.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    SidebarComponent
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
